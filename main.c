ef264360c3077ab1dd0bcb48626641ddb7668a1e
federicavil <federica_vil@yahoo.it>
2023-05-05 04:47:33 -0400

    creazione file risultati

--- main.c
+++ main.c
@@ -2,64 +2,74 @@
 #include <stdlib.h>
 #include "product/product_csr_openmp.h"
 #include <time.h>
+#include <math.h>
 
-#define NFILES 8
+#define NFILES 20
+#define NREPETITIONS 50
+#define FILEHEADER "MatrixName, NRows, NCol, NZ, k, FLOPS\n"
 
 void main(){
     coo_matrix mat;
     csr_matrix converted_matrix;
     matrix multivector, result;
     clock_t begin, end;
+    double timeSumSer = 0.0, timeSumCsrOmp = 0.0;
     int col_multivector[7] = {3,4,8,12,16,32,64}; 
 
+    FILE *resultsSer = fopen("measurements/results/serial.csv", "w+");
+    FILE *resultsCsrOmp = fopen("measurements/results/crsomp.csv", "w+");
+
+    if(resultsSer == NULL || resultsCsrOmp == NULL )
+        printf("errore apertura file\n");
+
     char* matFiles[] = {
-        "test_matrices/matrix_files/cage4.mtx",
-        "test_matrices/matrix_files/adder_dcop_32.mtx",
-        "test_matrices/matrix_files/af23560.mtx",
-        "test_matrices/matrix_files/bcsstk17.mtx",
-        "test_matrices/matrix_files/cant.mtx",
-        "test_matrices/matrix_files/cavity10.mtx",
-        "test_matrices/matrix_files/cop20k_A.mtx",
-        "test_matrices/matrix_files/Cube_Coup_dt0.mtx",
-        "test_matrices/matrix_files/lung2.mtx",
-        "test_matrices/matrix_files/mac_econ_fwd500.mtx",
-        "test_matrices/matrix_files/mcfe.mtx",
-        "test_matrices/matrix_files/mhd4800a.mtx",
-        "test_matrices/matrix_files/mhda416.mtx",
-        "test_matrices/matrix_files/ML_Laplace.mtx",
-        "test_matrices/matrix_files/olafu.mtx",
-        "test_matrices/matrix_files/olm1000.mtx",
-        "test_matrices/matrix_files/PRO2R.mtx",
-        "test_matrices/matrix_files/west2021.mtx",
-        "test_matrices/matrix_files/rdist2.mtx",
-        "test_matrices/matrix_files/raefsky2.mtx",
+        "matrices/matrix_files/cage4.mtx",
+        "matrices/matrix_files/olm1000.mtx",
+        "matrices/matrix_files/west2021.mtx",
+        "matrices/matrix_files/mhda416.mtx",
+        "matrices/matrix_files/adder_dcop_32.mtx",
+        "matrices/matrix_files/mcfe.mtx",
+        "matrices/matrix_files/rdist2.mtx",
+        "matrices/matrix_files/cavity10.mtx",
+        "matrices/matrix_files/mhd4800a.mtx",
+        "matrices/matrix_files/bcsstk17.mtx",
+        "matrices/matrix_files/raefsky2.mtx",
+        "matrices/matrix_files/af23560.mtx",
+        "matrices/matrix_files/olafu.mtx",
+        "matrices/matrix_files/lung2.mtx",
+        "matrices/matrix_files/cop20k_A.mtx",
+        "matrices/matrix_files/mac_econ_fwd500.mtx",
+        "matrices/matrix_files/cant.mtx",
+        "matrices/matrix_files/PR02R.mtx",
+        "matrices/matrix_files/ML_Laplace.mtx",
+        "matrices/matrix_files/Cube_Coup_dt0.mtx",
     }; 
-    
+
+    fprintf(resultsSer,"%s",FILEHEADER);
+    fprintf(resultsCsrOmp,"%s",FILEHEADER);
     for(int i = 0; i < NFILES; i++){
         printf("%s\n",matFiles[i]);
         mat = getMatrix(matFiles[i]);
         converted_matrix = convertToCsrFromCoo(mat);
-        //stampaMatriceCsr(converted_matrix);
-
-        for(int i = 0; i < 7; i++){
-            printf("COLONNE MULTIVETTORE %d\n",col_multivector[i]);
-            multivector = GenerateMultivector(mat.n,col_multivector[i]); 
-            begin = clock();
+        for(int j = 0; j < 7; j++){
+            multivector = GenerateMultivector(mat.n,col_multivector[j]);
             result = prepara_risultato(converted_matrix.m, multivector.n);
-            calcola_prodotto_seriale(converted_matrix,multivector, &result);
-            end = clock();
-            printf("\tserial %f\n",(double)(end - begin) / CLOCKS_PER_SEC);
-            // Stampa della matrice
-            //stampaMatrice(converted_matrix.m, vector.n,result);
-            begin = clock();
-            calcola_prodotto_per_righe_csr_openmp(converted_matrix,multivector, &result);
-            end = clock();
-            printf("\tparall %f\n", (double)(end - begin) / CLOCKS_PER_SEC);
-            // Stampa della matrice
-            //stampaMatrice(converted_matrix.m, vector.n,result);
-    
+            for(int k = 0; k < NREPETITIONS; k++){
+                begin = clock();
+                calcola_prodotto_seriale(converted_matrix,multivector, &result);
+                end = clock();
+                timeSumSer += (double)(end - begin) / CLOCKS_PER_SEC;
+                begin = clock();
+                calcola_prodotto_per_righe_csr_openmp(converted_matrix,multivector, &result);
+                end = clock();
+                timeSumCsrOmp += (double)(end - begin) / CLOCKS_PER_SEC;
+            }
+        fprintf(resultsSer,"%s, %d, %d, %d, %d, %f\n",matFiles[i],converted_matrix.m, converted_matrix.n, converted_matrix.nz, col_multivector[j], ((2*col_multivector[j]*converted_matrix.nz)/(timeSumSer/NREPETITIONS))/pow(10,6));
+        fprintf(resultsCsrOmp,"%s, %d, %d, %d, %d, %f\n",matFiles[i],converted_matrix.m, converted_matrix.n, converted_matrix.nz, col_multivector[j], ((2*col_multivector[j]*converted_matrix.nz)/(timeSumCsrOmp/NREPETITIONS))/pow(10,6)); 
         }
     }
+    fclose(resultsSer);
+    fclose(resultsCsrOmp);
     /*
     mat = getMatrix("test_matrices/matrix_files/prova_simm.mtx");
     
